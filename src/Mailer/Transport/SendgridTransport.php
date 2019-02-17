8c39ec3c2812e68aff4ef9b556e5fe981e71b6b1
Marek Bugla <buglamarek@gmail.com>
2019-02-17 21:21:14 +0100

    add events handling

--- src/Mailer/Transport/SendgridTransport.php
+++ src/Mailer/Transport/SendgridTransport.php
@@ -5,7 +5,9 @@ namespace Mailer\Transport;
 use Mailer\SendGrid\SendGridClient;
 use Mailer\SendGrid\SwiftToSendGridMessageTranslator;
 use SendGrid\Mail\Mail;
+use Swift_Events_EventDispatcher;
 use Swift_Events_EventListener;
+use Swift_Events_SendEvent;
 use Swift_Mime_SimpleMessage;
 
 class SendgridTransport implements \Swift_Transport
@@ -18,14 +20,22 @@ class SendgridTransport implements \Swift_Transport
     /** @var SwiftToSendGridMessageTranslator */
     private $swiftToSendGridMessageTranslator;
 
+    /** @var Swift_Events_EventDispatcher */
+    private $eventDispatcher;
+
     /**
      * @param SendGridClient $sendGridClient
      * @param SwiftToSendGridMessageTranslator $swiftToSendGridMessageTranslator
+     * @param Swift_Events_EventDispatcher $dispatcher
      */
-    public function __construct(SendGridClient $sendGridClient, SwiftToSendGridMessageTranslator $swiftToSendGridMessageTranslator)
-    {
+    public function __construct(
+        SendGridClient $sendGridClient,
+        SwiftToSendGridMessageTranslator $swiftToSendGridMessageTranslator,
+        Swift_Events_EventDispatcher $dispatcher
+    ) {
         $this->sendGridClient = $sendGridClient;
         $this->swiftToSendGridMessageTranslator = $swiftToSendGridMessageTranslator;
+        $this->eventDispatcher = $dispatcher;
     }
 
     /**
@@ -95,6 +105,17 @@ class SendgridTransport implements \Swift_Transport
      */
     public function send(Swift_Mime_SimpleMessage $message, &$failedRecipients = null)
     {
+        if (!$this->isStarted()) {
+            $this->start();
+        }
+
+        if ($evt = $this->eventDispatcher->createSendEvent($this, $message)) {
+            $this->eventDispatcher->dispatchEvent($evt, 'beforeSendPerformed');
+            if ($evt->bubbleCancelled()) {
+                return 0;
+            }
+        }
+
         $sentMails = 0;
 
         try {
@@ -107,16 +128,26 @@ class SendgridTransport implements \Swift_Transport
                     count($message->getTo() ?? [])
                     + count($message->getCc() ?? [])
                     + count($message->getBcc() ?? []);
+
+                if ($evt) {
+                    $evt->setResult(Swift_Events_SendEvent::RESULT_SUCCESS);
+                }
             } else {
-                $failedRecipients = array_merge(
-                    (array)$message->getTo() ,
-                    (array)$message->getCc(),
-                    (array)$message->getBcc(),
-                    (array)$failedRecipients
-                );
+                $this->fillFailedRecipients($message, $failedRecipients);
+
+                if ($evt) {
+                    $evt->setResult(Swift_Events_SendEvent::RESULT_FAILED);
+                    $evt->setFailedRecipients($failedRecipients);
+                }
             }
         } catch (\Exception $e) {
-            //TODO: Add some logging
+            if ($evt) {
+                $evt->setResult(Swift_Events_SendEvent::RESULT_FAILED);
+            }
+        }
+
+        if($evt) {
+            $this->eventDispatcher->dispatchEvent($evt, 'sendPerformed');
         }
 
         return $sentMails;
@@ -129,7 +160,7 @@ class SendgridTransport implements \Swift_Transport
      */
     public function registerPlugin(Swift_Events_EventListener $plugin)
     {
-
+        $this->eventDispatcher->bindEventListener($plugin);
     }
 
     /**
@@ -139,5 +170,22 @@ class SendgridTransport implements \Swift_Transport
     private function isSuccessful(\SendGrid\Response $response)
     {
         return $response->statusCode() < 300 && $response->statusCode() > 199;
+    }
+
+    /**
+     * @param Swift_Mime_SimpleMessage $message
+     * @param $failedRecipients
+     * @return array
+     */
+    public function fillFailedRecipients(Swift_Mime_SimpleMessage $message, &$failedRecipients): array
+    {
+        $failedRecipients = array_merge(
+            (array)$message->getTo(),
+            (array)$message->getCc(),
+            (array)$message->getBcc(),
+            (array)$failedRecipients
+        );
+
+        return $failedRecipients;
     }
 }
--- tests/Mailer/Transport/SendgridTransportTest.php
+++ tests/Mailer/Transport/SendgridTransportTest.php
@@ -4,17 +4,21 @@ namespace Test\Mailer\Transport;
 
 use Mailer\SendGrid\SwiftToSendGridMessageTranslator;
 use Mailer\Transport\SendgridTransport;
+use Prophecy\Argument;
+use Swift_Events_EventDispatcher;
 use Test\Mailer\SendGrid\Fake\AlwaysFailingSendGridClient;
 use Test\Mailer\SendGrid\Fake\AlwaysSuccessSendGridClient;
 
 class SendgridTransportTest extends \PHPUnit\Framework\TestCase
 {
+    protected $eventDispatcher;
+
     /**
      * @test
      */
     public function canBeStarted()
     {
-        $transport = new SendgridTransport($this->getSuccessClient(), new SwiftToSendGridMessageTranslator());
+        $transport = $this->getTransport();
 
         $transport->start();
 
@@ -26,7 +30,7 @@ class SendgridTransportTest extends \PHPUnit\Framework\TestCase
      */
     public function canBeStopped()
     {
-        $transport = new SendgridTransport($this->getSuccessClient(), new SwiftToSendGridMessageTranslator());
+        $transport = $this->getTransport();
 
         $transport->stop();
 
@@ -38,7 +42,7 @@ class SendgridTransportTest extends \PHPUnit\Framework\TestCase
      */
     public function canBePinged()
     {
-        $transport = new SendgridTransport($this->getSuccessClient(), new SwiftToSendGridMessageTranslator());
+        $transport = $this->getTransport();
 
         $this->assertTrue($transport->ping());
     }
@@ -48,17 +52,26 @@ class SendgridTransportTest extends \PHPUnit\Framework\TestCase
      */
     public function isAbleToSendMessage()
     {
-       $message = (new \Swift_Message('The Message'))
+        $message = (new \Swift_Message('The Message'))
             ->setFrom('john@doe.com')
             ->setTo('jane@doe.com')
-            ->setBody('Something really important', 'text/plain')
-        ;
+            ->setBody('Something really important', 'text/plain');
 
-        $transport = new SendgridTransport($this->getSuccessClient(), new SwiftToSendGridMessageTranslator());
+        $transport = $this->getTransport();
 
         $sentMails = $transport->send($message);
 
         $this->assertEquals(1, $sentMails);
+
+        $this->eventDispatcher->dispatchEvent(
+            Argument::type(\Swift_Events_SendEvent::class),
+            Argument::exact('beforeSendPerformed')
+        )->shouldHaveBeenCalledTimes(1);
+
+        $this->eventDispatcher->dispatchEvent(
+            Argument::type(\Swift_Events_SendEvent::class),
+            Argument::exact('sendPerformed')
+        )->shouldHaveBeenCalledTimes(1);
     }
 
     /**
@@ -70,10 +83,9 @@ class SendgridTransportTest extends \PHPUnit\Framework\TestCase
             ->setFrom('john@doe.com')
             ->setTo('jane@doe.com')
             ->setCc(['will@smith.com', 'bruceLee@kung.fu'])
-            ->setBody('Something really important', 'text/plain')
-        ;
+            ->setBody('Something really important', 'text/plain');
 
-        $transport = new SendgridTransport($this->getSuccessClient(), new SwiftToSendGridMessageTranslator());
+        $transport = $this->getTransport();
 
         $sentMails = $transport->send($message);
 
@@ -89,10 +101,9 @@ class SendgridTransportTest extends \PHPUnit\Framework\TestCase
             ->setFrom('john@doe.com')
             ->setTo('jane@doe.com')
             ->setBcc(['will@smith.com', 'bruceLee@kung.fu'])
-            ->setBody('Something really important', 'text/plain')
-        ;
+            ->setBody('Something really important', 'text/plain');
 
-        $transport = new SendgridTransport($this->getSuccessClient(), new SwiftToSendGridMessageTranslator());
+        $transport = $this->getTransport();
 
         $sentMails = $transport->send($message);
 
@@ -108,15 +119,24 @@ class SendgridTransportTest extends \PHPUnit\Framework\TestCase
             ->setFrom('john@doe.com')
             ->setTo('jane@doe.com')
             ->setBcc(['will@smith.com', 'bruceLee@kung.fu'])
-            ->setBody('Something really important', 'text/plain')
-        ;
+            ->setBody('Something really important', 'text/plain');
 
-        $transport = new SendgridTransport($this->getFailingClient(), new SwiftToSendGridMessageTranslator());
+        $transport = $this->getTransport('failing');
 
-        $sentMails = $transport->send($message,$failedRecipients);
+        $sentMails = $transport->send($message, $failedRecipients);
 
         $this->assertEquals(0, $sentMails);
         $this->assertCount(3, $failedRecipients);
+
+        $this->eventDispatcher->dispatchEvent(
+            Argument::type(\Swift_Events_SendEvent::class),
+            Argument::exact('beforeSendPerformed')
+        )->shouldHaveBeenCalledTimes(1);
+
+        $this->eventDispatcher->dispatchEvent(
+            Argument::type(\Swift_Events_SendEvent::class),
+            Argument::exact('sendPerformed')
+        )->shouldHaveBeenCalledTimes(1);
     }
 
     /**
@@ -130,5 +150,44 @@ class SendgridTransportTest extends \PHPUnit\Framework\TestCase
     public function getFailingClient(): AlwaysFailingSendGridClient
     {
         return new AlwaysFailingSendGridClient(123);
+    }
+
+    /**
+     * @param string $clientType
+     *
+     * @return SendgridTransport
+     */
+    public function getTransport($clientType = 'success'): SendgridTransport
+    {
+        switch ($clientType) {
+            default:
+            case 'success':
+                $client = $this->getSuccessClient();
+                break;
+            case 'failing':
+                $client = $this->getFailingClient();
+                break;
+        }
+
+        $this->eventDispatcher = $this->prophesize(Swift_Events_EventDispatcher::class);
+
+        $event = $this->prophesize(\Swift_Events_SendEvent::class);
+        $this->eventDispatcher->createSendEvent(
+            Argument::type(\Swift_Transport::class),
+            Argument::type(\Swift_Message::class)
+        )->willReturn($event->reveal());
+
+        $this->eventDispatcher->dispatchEvent(
+            Argument::type(\Swift_Events_SendEvent::class),
+            Argument::type('string')
+        )->willReturn(null);
+
+        $transport = new SendgridTransport(
+            $client,
+            new SwiftToSendGridMessageTranslator(),
+            $this->eventDispatcher->reveal()
+        );
+
+        return $transport;
     }
 }
